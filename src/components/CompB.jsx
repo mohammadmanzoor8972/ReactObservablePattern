3d54ab859493d3fa98232c2e5df36f2ced039a6b
Mohammad Manzoor Alam <mohammad.manzoor8972@gmail.com>
2019-11-20 18:24:24 +0530

    direction
    bi-directional

--- src/components/CompB.jsx
+++ src/components/CompB.jsx
@@ -8,10 +8,15 @@ const CompB = props => {
     setMessage(data);
   });
 
+  const handler = () => {
+    Observable.broadcast("From Component B");
+  };
+
   return (
     <div style={{ width: "300px", border: "1px solid black", height: "250px" }}>
       <h1>Component B</h1>
       {message}
+      <button onClick={handler}>Click Me</button>
     </div>
   );
 };
